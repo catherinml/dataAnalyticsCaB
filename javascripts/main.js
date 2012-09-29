9e53ca366c6bc4e61a7de007b43e88049d52bd05
Mary Catherine Thomas <kaatherin@yahoo.com>
2012-09-29 17:19:07 +0530

    Modified file

--- javascripts/main.js
+++ javascripts/main.js
@@ -4462,37 +4462,34 @@ var json = {"graphNodeDetails":
 	gadgets.window.adjustHeight();
 	$("#chart").html("");
 try {
-	 	var margin = {top: 100, right: 800, bottom: 400, left: 100},
-	    	width = 960 - margin.left - margin.right,
-	    	height = 500 - margin.top - margin.bottom;
+	var margin = {top: 100, right: 800, bottom: 400, left: 100},
+	    width = 960 - margin.left - margin.right,
+	    height = 500 - margin.top - margin.bottom;
 
 	 	var x = d3.scale.linear()
-	    		.range([0, width]);
+	    .range([0, width]);
 
-		var y = d3.scale.linear()
-	    		.range([height, 0]);
+	var y = d3.scale.linear()
+	    .range([height, 0]);
 		 	
 
-		var color = d3.scale.category20();
+	var color = d3.scale.category20();
 
 
-		var force = d3.layout.force()
-			//.gravity(0.06)
-	    		.charge(-120)
-	   		// .linkDistance(30)
-	    		.size([width, height]);
+	var force = d3.layout.force()
+		//.gravity(0.06)
+	    .charge(-120)
+	   // .linkDistance(30)
+	    .size([width, height]);
 
-		var svg = d3.select("#chart").append("svg")
-	    		.attr("width", width + margin.left + margin.right)
-	    		.attr("height", height + margin.top + margin.bottom)
-	  		.append("svg:g")
-	    		//.attr("transform", "translate(" + width / 4 + "," + height / 3 + ")");
-	    		.attr("transform", "translate(" + margin.left + "," + margin.top +")");
-    			//.call(d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", 10))
-    			//.append("svg:g");
-
-		// d3.json("http://localhost:8080/JiveDataVizsWeb/jivegraph/dataviz/nodes", function(json) {
-	  	//d3.json("mynodes2.json", function(json) {
+	var svg = d3.select("#chart").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  	.append("svg:g")
+	    //.attr("transform", "translate(" + width / 4 + "," + height / 3 + ")");
+	    .attr("transform", "translate(" + margin.left + "," + margin.top +")");
+    	//.call(d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", 10))
+    	//.append("svg:g");
 		
 
 	  	force
@@ -4501,36 +4498,36 @@ try {
 	      	.linkDistance(function(d) { return (1/d.value) * 100; })   
 	      	.start();
 	 
-	  	var link = svg.selectAll("line.link")
-	        .data(json.graphNodeDetails.links);
-	    	link.enter().append("line")
-	        .attr("class", "link")
-	        .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+	  	 var link = svg.selectAll("line.link")
+	      .data(json.graphNodeDetails.links);
+	    link.enter().append("line")
+	      .attr("class", "link")
+	      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-	  	var node = svg.selectAll("circle.node")
-	        .data(json.graphNodeDetails.nodes);
-	    	node.enter().append("circle")
-	        .attr("class", "node")
-	        .attr("r", function(d) { return d.value; })
-	        .style("fill", function(d) { return color(d.group); })
-	     	// .on("mouseover", fade(.1, true))
-         	// .on("mouseout", normalizeNodesAndRemoveLabels())
-	        .call(force.drag);
+	  var node = svg.selectAll("circle.node")
+	      .data(json.graphNodeDetails.nodes);
+	    node.enter().append("circle")
+	      .attr("class", "node")
+	     // .attr("r", function(d) { return d.value/5; })
+	      .attr("r", 6)
+	      .style("fill", function(d) { return color(d.group); })
+	     // .on("mouseover", fade(.1, true))
+         // .on("mouseout", normalizeNodesAndRemoveLabels())
+	      .call(force.drag);
 
-	  	node.append("title")
-	        .text(function(d) { return d.name; });
+	  node.append("title")
+	      .text(function(d) { return d.name; });
 
-	  	force.on("tick", function() {
-	    		link.attr("x1", function(d) { return d.source.x; })
-	        	.attr("y1", function(d) { return d.source.y; })
-	        	.attr("x2", function(d) { return d.target.x; })
-	        	.attr("y2", function(d) { return d.target.y; });
+	  force.on("tick", function() {
+	    link.attr("x1", function(d) { return d.source.x; })
+	        .attr("y1", function(d) { return d.source.y; })
+	        .attr("x2", function(d) { return d.target.x; })
+	        .attr("y2", function(d) { return d.target.y; });
 
-	    	node.attr("cx", function(d) { return d.x; })
+	    node.attr("cx", function(d) { return d.x; })
 	        .attr("cy", function(d) { return d.y; });
 	  });
-	//});
-		 	
+	//});	 	
 		} catch(err) {
 			alert("Error" + err.message);
 		}
