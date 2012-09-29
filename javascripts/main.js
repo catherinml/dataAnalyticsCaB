f096322570da3a41659a6fded318765462004471
Mary Catherine Thomas <kaatherin@yahoo.com>
2012-09-29 14:25:17 +0530

    Modified file

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,7 +4,8 @@ gadgets.util.registerOnLoadHandler(function() {
 
   $("#typeofrelationship").change(function() {
 	var typeID = $("#typeofrelationship").val();
-	if (typeID == "@all" ) {
+	$("#selectgroup").html("");
+	if (typeID == "@self" ) {
 		osapi.groups.get({ 
 			userId : "@me", 
 			groupId : "@self", 
@@ -28,7 +29,32 @@ gadgets.util.registerOnLoadHandler(function() {
 				}
 			} 
 		});
-	}
+	} else if (typeID == "@all" ) {
+
+		osapi.groups.get({ 
+			userId : "@me", 
+			groupId : "@all", 
+			sortBy : "title", 
+			sortOrder : "ascending" ,
+			count : 10
+		}).execute(function (response) { 
+			if (response.error) { 
+				alert("Error " + response.error.code + " reading groups. Error message was: " + response.error.message); 
+			} 
+			else { 
+				if (response.list) {
+					var groups = response.list; 
+					var html = "";
+																	$(groups).each(function(index, group) 					{
+																		html += "<option value=" + group.id + ">" + group.title + "</option>";
+			         
+        				});
+					 																								$("#selectgroup").html(html);
+  					  												gadgets.window.adjustHeight();
+				}
+			} 
+		});
+	} // end else if
     });
 
  });
