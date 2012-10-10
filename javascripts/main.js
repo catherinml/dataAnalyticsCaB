39478307392cc90407308cbc1d8978a6d0e64380
Mary Catherine Thomas <kaatherin@yahoo.com>
2012-10-10 11:57:37 +0530

    Modified

--- javascripts/main.js
+++ javascripts/main.js
@@ -4591,6 +4591,66 @@ $("#displaygraph").click(function() {
 //});
 gadgets.window.adjustHeight();
 
- 	});
+});
 
- });
+
+
+	$("#getdata").click(function() {
+		gadgets.window.adjustHeight();
+		$("#chart").html("");
+		$("#graphdata").css("display","block");
+
+	});
+	
+	
+  $("#fromnode").click(function() {
+    var callback = function(response) {
+      var users = getUsersFromResponse(response);
+      renderUserTable(users);
+    }
+    osapi.jive.core.users.requestPicker({success: callback, multiple: false});
+  });
+
+});
+
+function getUsersFromResponse(response) {
+  var users = [];
+  if(response.data instanceof osapi.jive.core.User) {
+    users.push(response.data);
+  } else if (response.data instanceof Array) {
+    users = response.data;
+  }
+  return users;
+}
+
+
+function renderUserTable(users) {
+  var currentView = gadgets.views.getCurrentView().getName();
+
+  // render header row
+  var content = ""
+  content += "<tr>"
+  content += "<th>ID</th>";
+  content += "<th>Name</th>";
+  content += "<th>Username</th>";
+  // include email in canvas view
+  if (currentView == 'canvas' || currentView == 'default') // Jive 5 currently returns 'default' for the canvas view
+    content += "<th>Email</th>";
+  content += "</tr>";
+
+  // render user rows
+  for(var i = 0; i < users.length; i++) {
+    var user = users[i];
+    content += "<tr>"
+    content += "<td>" + user.id + "</td>";
+    content += "<td>" + user.name + "</td>";
+    content += "<td>" + user.username + "</td>";
+    // include email in canvas view
+    if (currentView == 'canvas' || currentView == 'default') // Jive 5 currently returns 'default' for the canvas view
+      content += "<td>" + user.email + "</td>";
+    content += "</tr>";
+  }
+
+  $("#user-from-content").html(content);
+   gadgets.window.adjustHeight();
+}
