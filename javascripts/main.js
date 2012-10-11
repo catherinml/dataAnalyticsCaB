20b629fb67e1e282401ca42b9c1706b0aaa41e0c
Mary Catherine Thomas <kaatherin@yahoo.com>
2012-10-11 17:31:47 +0530

    commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -4641,7 +4641,9 @@ gadgets.window.adjustHeight();
 				if(d.name == usertoid) {
 					
 					json.graphNodeDetails.links.forEach(function(d1) {
-						
+						alert(fromindex + " :" + d1.source);
+				alert(indexposto + " :" + d1.target);
+
 						if(d1.source == fromindex &&
 							d1.target == indexposto ) 							{
 
