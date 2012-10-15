9fe6eb7e3963478590ec642b6bc57f220b0c5746
Mary Catherine Thomas <kaatherin@yahoo.com>
2012-10-15 16:56:14 +0530

    updated

--- javascripts/main.js
+++ javascripts/main.js
@@ -4465,13 +4465,7 @@ $("input[type='radio']").change(function () {
 
 	$("#spacetable").css("display","block");
 	alert("hello");
-		osapi.groups.get({ 
-			userId : "@me", 
-			groupId : "@all", 
-			sortBy : "title", 
-			sortOrder : "ascending" ,
-			count : 20
-		}).execute(function (response) { 
+		osapi.jive.core.spaces.get({contextId: "@viewer", contextType: "@person"}).execute(function (response) { 
 			alert(response);
 			if (response.error) { 
 				alert("Error " + response.error.code + " reading groups. Error message was: " + response.error.message); 
