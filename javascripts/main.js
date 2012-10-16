b2a2f37917ebe39706babc4c3f7adcd7c411d87b
Mary Catherine Thomas <kaatherin@yahoo.com>
2012-10-16 12:04:23 +0530

    updated

--- javascripts/main.js
+++ javascripts/main.js
@@ -4466,7 +4466,7 @@ $("input[type='radio']").change(function () {
 	$("#spacetable").css("display","block");
 	
 		//osapi.jive.core.spaces.get({limit:30}).execute(function (response) { 
-		osapi.spaces.get({contextId: "@viewer", contextType: "@person"}).execute(function(response) {
+		osapi.jive.core.spaces.get({contextId: "@viewer", contextType: "@person"}).execute(function(response) {
 			alert(response.totalResults);
 			if (response.error) {
 				
