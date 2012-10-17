d17f6bce650ecab4876a9e12f5763bf669ba446f
Mary Catherine Thomas <kaatherin@yahoo.com>
2012-10-17 14:57:30 +0530

    updated

--- javascripts/main.js
+++ javascripts/main.js
@@ -4529,7 +4529,7 @@ $("#displaygraph").click(function() {
 params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON;
 gadgets.io.makeRequest("http://accenture-tle-uat.uat3.hosted.jivesoftware.com/api/core/v2/spaces",
 function(data){
-alert(data.message);
+alert(data.error.message);
 },
 params);
 
