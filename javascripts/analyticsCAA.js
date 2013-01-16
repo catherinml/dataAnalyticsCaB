99cd19c9367b35e4717878ffb3a9e2a351c24139
Mary Catherine Thomas <kaatherin@yahoo.com>
2013-01-16 17:27:37 +0530

    Modified

--- javascripts/analyticsCAA.js
+++ javascripts/analyticsCAA.js
@@ -8,6 +8,24 @@ gadgets.util.registerOnLoadHandler(function() {
 				var content = renderPlaceOnTable(data);
 				$("#caGroupName").html(content);
 				//var responseJson = gadgets.json.stringify(data);
+
+$(document).ready(function(){
+	
+	var instanceURL = "https://sandbox.jiveon.com";
+	$.ajax({
+		   
+		url: 'http://svecas001:8090/CollaborativeAwarenessApp/collabaware/ca/mostcontributed?url='+content+'&instanceURL='+instanceURL,
+	    dataType: 'json',
+	    type: 'GET',
+	    success: function(result) {
+	       
+	       	        alert(result.data);
+	    },
+	    error: function(XMLHttpRequest, textStatus, errorThrown) {
+	    			alert(errorThrown); 	
+	    }
+	});
+	
 			}
 		});
 	});
