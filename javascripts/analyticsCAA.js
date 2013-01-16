eb3a1928e50e8ab980251ae2b5c09d705fff493f
Mary Catherine Thomas <kaatherin@yahoo.com>
2013-01-16 16:57:35 +0530

    Modified

--- javascripts/analyticsCAA.js
+++ javascripts/analyticsCAA.js
@@ -3,16 +3,22 @@ gadgets.util.registerOnLoadHandler(function() {
 		osapi.jive.corev3.places.requestPicker({
 			type : "group",
 			success : function(data) {
-				alert(data.name);
-				var responseJson = gadgets.json.stringify(data);
-				alert(responseJson);
-
-				
-				
+				//alert(data.name);
+				var content = renderPlaceOnTable(data);
+				$("#caGroupName").html(content);
+				//var responseJson = gadgets.json.stringify(data);
 			}
 		});
 	});
 });
+
+function renderPlaceOnTable(place) {
+	var content = "";
+     
+     content += "<p>"+ place.name + '</p><input type="hidden" value="'+place.id + '">'
+     
+	return content ;
+}
 
 
 
