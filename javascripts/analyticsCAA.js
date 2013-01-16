8fadfce32b19bb34106cd7b9983529faf425ad63
Mary Catherine Thomas <kaatherin@yahoo.com>
2013-01-16 12:30:44 +0530

    Modified

--- javascripts/analyticsCAA.js
+++ javascripts/analyticsCAA.js
@@ -8,36 +8,13 @@ gadgets.util.registerOnLoadHandler(function() {
     		var callback = function(response) {
 				
 				alert("inside callback");
-	 			//var places = getPlaceFromResponse(response);
-				//alert("Places--" + places );
-      			//var content = renderPlaceOnTable(places);
-				//$("#caGroupName").html(content);
-				//gadgets.window.adjustHeight();
+
     		}
-     			osapi.jive.core.places.requestPicker({success:callback, placeType: "group"});
+     			osapi.jive.core.places.requestPicker({success:callback, placeType: "group,space"});
 			
 	});
 gadgets.window.adjustHeight();
 });
-
-	/*function getPlaceFromResponse(response) {
-		 alert("response length" + response);
-		  var places = [];
-		
-		  return places;
-	}
-	
-	function renderPlaceOnTable(places) {
-		 var place;
-		 var content = "";
-		
-		 for(var i = 0; i < places.length; i++) {
-    		place = places[i];
-    		content += "<p>"+ place.name + '</p><input type="hidden" value="'+place.id + '">'
-    	}
-		return content ;
-	}
-*/
 
 
 
