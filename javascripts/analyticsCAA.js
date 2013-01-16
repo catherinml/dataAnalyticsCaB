bfc1fae4c23355d633802d03deff5e97df8acbe4
Mary Catherine Thomas <kaatherin@yahoo.com>
2013-01-16 11:16:50 +0530

    Modified

--- javascripts/analyticsCAA.js
+++ javascripts/analyticsCAA.js
@@ -10,7 +10,7 @@ gadgets.util.registerOnLoadHandler(function() {
 				$("#caGroupName").html(content);
 				gadgets.window.adjustHeight();
     		}
-     			osapi.jive.core.places.requestPicker({success:callback, placeType: "group"});
+     			osapi.jive.core.places.requestPicker({success:callback, placeType: "@all"});
 	});
 });
 
