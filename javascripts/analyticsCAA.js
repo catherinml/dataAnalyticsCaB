47cfd3f6a4ed69e893e67772cc9f17cd64a95f27
Mary Catherine Thomas <kaatherin@yahoo.com>
2013-01-16 11:14:35 +0530

    Modified

---
+++ javascripts/analyticsCAA.js
@@ -0,0 +1,42 @@
+
+
+gadgets.util.registerOnLoadHandler(function() {
+
+	$("#caSelectGroup").click(function() {
+		
+    		var callback = function(response) {
+	 			var places = getPlaceFromResponse(response);
+      			var content = renderPlaceOnTable(places);
+				$("#caGroupName").html(content);
+				gadgets.window.adjustHeight();
+    		}
+     			osapi.jive.core.places.requestPicker({success:callback, placeType: "group"});
+	});
+});
+
+	function getPlaceFromResponse(response) {
+		  var places = [];
+		  if(response.data instanceof osapi.jive.corev3.places) {
+		    	places.push(response.data);
+		  } else if (response.data instanceof Array) {
+		    	places= response.data;
+		  }
+		  return places;
+	}
+	
+	function renderPlaceOnTable(places) {
+		 var place;
+		 var content = "";
+		 for(var i = 0; i < places.length; i++) {
+    		place = places[i];
+    		content += "<p>"+ place.name + '</p><input type="hidden" value="'+place.id + '">'
+    	}
+		return content ;
+	}
+
+
+
+
+
+
+
