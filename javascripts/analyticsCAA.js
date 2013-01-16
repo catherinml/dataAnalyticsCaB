b88700fa3af46c00430aeb370ff98a3055bd82fb
Mary Catherine Thomas <kaatherin@yahoo.com>
2013-01-16 15:59:22 +0530

    Modified

--- javascripts/analyticsCAA.js
+++ javascripts/analyticsCAA.js
@@ -1,7 +1,7 @@
 gadgets.util.registerOnLoadHandler(function() {
 	$("#caSelectGroup").click(function() {
 		osapi.jive.corev3.places.requestPicker({
-			type : "space",
+			type : "space,group",
 			success : function(data) {
 				alert(data);
 			}
