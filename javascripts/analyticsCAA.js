0de47f5eabe45c3b6f4c79e1f1684d8568708ce2
Mary Catherine Thomas <kaatherin@yahoo.com>
2013-01-16 14:42:56 +0530

    Modified

--- javascripts/analyticsCAA.js
+++ javascripts/analyticsCAA.js
@@ -1,21 +1,5 @@
 
 
-gadgets.util.registerOnLoadHandler(function() {
-	alert("About to call---");
-
-	
-	$("#caSelectGroup").click(function() {
-		
-    		var callback = function(response) {
-				
-				alert("inside callback");
-
-    		}
-     			osapi.jive.core.places.requestPicker({success:callback, placeType: "space"});
-			
-	});
-gadgets.window.adjustHeight();
-});
 
 
 
