61269025f83484518fc989666b6523fbffc13e1a
Mary Catherine Thomas <kaatherin@yahoo.com>
2013-01-17 13:01:40 +0530

    Modified

--- javascripts/analyticsCAA.js
+++ javascripts/analyticsCAA.js
@@ -17,7 +17,8 @@ gadgets.util.registerOnLoadHandler(function() {
 	
 					$.ajax({
 						   
-						url: 'http://svecas001:8090/CollaborativeAwarenessApp/collabaware/ca/mostcontributed/jsonP?url='+data.resources.activity.ref+'&instanceURL='+instanceURL+'&callback=',
+						//url: 'http://svecas001:8090/CollaborativeAwarenessApp/collabaware/ca/mostcontributed/jsonP?url='+data.resources.activity.ref+'&instanceURL='+instanceURL+'&callback=',
+						url:'https://ajax.googleapis.com/ajax/services/search/books?v=1.0&q=jquery',
 					    dataType: 'jsonp',
 					    type: 'GET',
 					    crossDomain:true,
