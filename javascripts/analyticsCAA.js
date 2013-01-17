467c978a1c1ad625c504f8cbd7b69c24aced55bb
Mary Catherine Thomas <kaatherin@yahoo.com>
2013-01-17 10:35:54 +0530

    Modified

--- javascripts/analyticsCAA.js
+++ javascripts/analyticsCAA.js
@@ -1,41 +1,32 @@
 gadgets.util.registerOnLoadHandler(function() {
-$("#caSelectGroup").click(function() {
-osapi.jive.corev3.places.requestPicker({
-type : "group",
-success : function(data) {
-alert(data.resources.activity.ref);
-alert(gadgets.json.stringify(data));
-var content = renderPlaceOnTable(data);
-$("#caGroupName").html(content);
-//var responseJson = gadgets.json.stringify(data);
+	$("#caSelectGroup").click(function() {
+		$(document).ready(function(){
+	
+	
+					$.ajax({
+						   
+						url: 'http://svecas001:8090/CollaborativeAwarenessApp/collabaware/ca/mostcontributed?url='+data.resources.activity.ref+'&instanceURL='+instanceURL,
+					    dataType: 'json',
+					    type: 'GET',
+					    success: function(result) {
+					       			alert("success");
+					       	        //displayCAData(result.data);
+					    },
+					    error: function(XMLHttpRequest, textStatus, errorThrown) {
+								    alert("error" + textStatus + "   : " + XMLHttpRequest.responseText);
+		
+					    			alert(errorThrown); 	
+					    }
+					});
+				});
 
-$(document).ready(function(){
-
-var instanceURL = "https://sandbox.jiveon.com/api/core/v3/";
-$.ajax({
-
-url: 'http://svecas001:8090/CollaborativeAwarenessApp/collabaware/jivegroups/all?count=1&instanceURL='+instanceURL,
-dataType: 'json',
-type: 'GET',
-success: function(result) {
-
-alert(result.data);
-},
-error: function(XMLHttpRequest, textStatus, errorThrown) {
-alert(errorThrown);
-}
-});
-});
-}
-});
-});
+	});
 });
 
-function renderPlaceOnTable(place) {
-var content = "";
-     
-     content += "<p>"+ place.name + '</p><input type="hidden" value="'+place.id + '">'
-     
-return content ;
-}
-
+	function renderPlaceOnTable(place) {
+		var content = "";
+	     
+	     content += "<p>"+ place.name + '</p><input type="hidden" value="'+place.id + '">'
+	     
+		return content ;
+	}
