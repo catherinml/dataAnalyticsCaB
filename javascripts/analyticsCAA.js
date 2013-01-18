d3ee806ed7ee18aa23660075a86ad525b5137b90
Mary Catherine Thomas <kaatherin@yahoo.com>
2013-01-18 10:01:04 +0530

    Modified

--- javascripts/analyticsCAA.js
+++ javascripts/analyticsCAA.js
@@ -137,7 +137,7 @@ gadgets.util.registerOnLoadHandler(function() {
 		var content="";
 		content += "<tr>";
 		
-		content += '<td style="text-align:center;">' + '<img src="ajax-loader.gif" /> </td>';
+		content += '<td style="text-align:center;">' + '<img src="https://raw.github.com/catherinml/dataAnalyticsCaA/master/images/ajax-loader.gif" /> </td>';
 		content += "</tr>";
 		$("#user-ca-content").html(content);
 	}
